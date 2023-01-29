468a9c27696bbb86e3d94c6ef30fbe5d75ce8309
Mahmoud Ibrahim <mahmoudewida3@gmail.com>
2023-01-29 23:14:30 +0200

    Add mainBtn

--- app/src/main/java/com/ewida/rickmorti/MainActivity.kt
+++ app/src/main/java/com/ewida/rickmorti/MainActivity.kt
@@ -2,6 +2,7 @@ package com.ewida.rickmorti
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ewida.rickmorti.custom_view.MainBtn
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
@@ -9,5 +10,10 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btn:MainBtn=findViewById(R.id.startbtn)
+        btn.setOnClickListener {
+            btn.changeLoading(1)
+        }
     }
 }
---
+++ app/src/main/java/com/ewida/rickmorti/custom_view/MainBtn.kt
@@ -0,0 +1,55 @@
+package com.ewida.rickmorti.custom_view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.View
+import android.widget.ProgressBar
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.ewida.rickmorti.R
+
+class MainBtn(context: Context, attributeSet: AttributeSet) :
+    ConstraintLayout(context, attributeSet) {
+
+    private var btnTv: TextView
+    private var btnPb: ProgressBar
+    private var mainBtn: ConstraintLayout
+
+    init {
+        inflate(context, R.layout.main_btn_layout, this)
+        val attrs = context.obtainStyledAttributes(attributeSet, R.styleable.MainBtn)
+        btnTv = findViewById(R.id.mainBtnTv)
+        btnPb = findViewById(R.id.mainBtnPb)
+        mainBtn = findViewById(R.id.mainBtn)
+        btnTv.text = attrs.getString(R.styleable.MainBtn_btnText)
+        changeLoading(0)
+        attrs.recycle()
+    }
+
+    fun changeLoading(state: Int) {
+        when (state) {
+            1 -> {
+                btnPb.visibility = View.VISIBLE
+                btnTv.visibility = View.INVISIBLE
+                isBtnEnabled(false)
+            }
+            0 -> {
+                btnTv.visibility = View.VISIBLE
+                btnPb.visibility = View.INVISIBLE
+                isBtnEnabled(true)
+            }
+        }
+    }
+
+    private fun isBtnEnabled(state: Boolean) {
+        if (state) {
+            mainBtn.setBackgroundResource(R.drawable.main_btn_background)
+            mainBtn.isClickable = false
+
+        } else {
+            mainBtn.setBackgroundResource(R.drawable.disabled_main_btn_background)
+            mainBtn.isClickable = true
+
+        }
+    }
+}
